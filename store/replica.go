78c81f0d7be503c38f54a7ec5b548335d45fd78e
ac5tin <austinchang4@gmail.com>
2020-09-26 10:46:26 +0800

    replicate on add server

--- store/replica.go
+++ store/replica.go
@@ -15,11 +15,43 @@ var replicas = []string{}
 // AddReplicaServer - add replica server
 func (s *Store) AddReplicaServer(server string) {
 	replicas = append(replicas, server)
+	go s.replicateSingleServer(server)
 }
 
 // RmReplicaServer - remove replica server
 func (s *Store) RmReplicaServer(server string) {
 	uf.ArrRMS(&replicas, server)
+}
+
+func (s *Store) replicateSingleServer(server string) error {
+	b, err := s.GetBinary()
+	if err != nil {
+		return err
+	}
+	var buf bytes.Buffer
+	xw, err := xz.NewWriter(&buf)
+	if err != nil {
+		return err
+	}
+	if _, err := xw.Write(b); err != nil {
+		return err
+	}
+	if err := xw.Close(); err != nil {
+		return err
+	}
+
+	opts := grpc.WithInsecure()
+	conn, err := grpc.Dial(server, opts)
+	if err != nil {
+		return err
+	}
+	client := quickkvpb.NewReplicaServiceClient(conn)
+	request := &quickkvpb.Data{Binary: buf.Bytes()}
+	_, err = client.Replicate(context.Background(), request)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // Replicate - start replication
